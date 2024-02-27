9ec81177938efe4ba1b3ac32acd7963c77c804a3
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2024-02-27 11:06:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//var altura = 5;
+//var comprimento = 7;
+
+//var area = altura * comprimento;
+
+
+//var area;
+
+let altura = 3;
+let comprimento = 5;
+let forma = 'retangulo';
+
+if  (forma === 'retangulo') {
+    area = altura * comprimento;
+} else {
+    (altura * comprimento) / 2; 
+}
+console.log(area)
