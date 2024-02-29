91b6ec21ffc15c19fdbf4e663ba67d91bccd53f6
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2024-02-29 12:27:59 -0300

    Add files via upload

---
+++ conversoes.js
@@ -0,0 +1,34 @@
+//coversao implicita
+
+
+/*const numero = 456;
+const numeroString = "456";
+
+console.log(numero === numeroString);*/
+
+/////////////////////////////////////////
+
+/*const numero = 456;
+const numeroString = "456";
+
+console.log(numero == numeroString);*/
+
+/////////////////////////////////////////
+
+//number()
+//string()
+/*
+const numero = 456;
+const numeroString = Number("456");
+
+console.log(numero / numeroString);*/
+
+////////////////////////////////////////////
+/*
+let telefone = 12341234;
+console.log("O telefone é " + String(telefone));*/
+
+////////////////////////////////////////////
+
+let telefone = 12341234;
+console.log("O telefone é " + telefone.toString());
