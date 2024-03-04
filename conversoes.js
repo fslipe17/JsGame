2b5fb290556849bd31d58fd7807aef51d3e9c3b1
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2024-03-04 11:39:26 -0300

    Add files via upload

--- conversoes.js
+++ conversoes.js
@@ -29,6 +29,26 @@ let telefone = 12341234;
 console.log("O telefone é " + String(telefone));*/
 
 ////////////////////////////////////////////
-
+/*
 let telefone = 12341234;
-console.log("O telefone é " + telefone.toString());
+console.log("O telefone é " + telefone.toString());*/
+
+///////////////////////////////////////////
+
+/*let usuarioConectado = false;
+console.log(String(usuarioConectado)); 
+usuarioConectado = true;
+console.log(String(usuarioConectado)); */
+
+///////////////////////////////////////////////
+
+/*
+let largura = "10";
+let altura = "5";
+console.log(Number(largura) * Number(altura));*/
+
+//////////////////////////////////////////////////
+
+let largura = "10";
+let altura = "5";
+console.log( + largura * + altura);
