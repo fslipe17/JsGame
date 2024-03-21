3570aedd293bfc8510f1cbef24dbe5d36d9b6525
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2024-03-21 12:26:12 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,31 @@
+// const notas = [10, 6.5, 8, 7.5];
+
+// const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+// console.log(media);
+
+// console.log(notas[3]);
+
+// notas.push(9);
+
+/////////////////////////////////////////////////
+
+// const notas = [10, 6, 8];
+
+// notas.push(7);
+
+// const media = (notas[0] + notas[1] + notas[2]) / notas.length;
+
+// console.log(media);
+
+/////////////////////////////////////////////////
+
+const notas = [10, 6, 8];
+
+notas.push(7);
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media);
+
+////////////////////////////////////////////////
