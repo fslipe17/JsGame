dc8bf0ef34c51a9381168952630bf75b421d6e8f
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2024-04-05 10:47:43 -0300

    Add files via upload

--- ExerciciosAula1/ex.js
+++ ExerciciosAula1/ex.js
@@ -1,4 +1,45 @@
-var nome = "Luiz";
-var idade = 17;
 
-console.log(`Oi, me chamo ${nome} e tenho ${idade} anos.`);
+// var nwme = "Luiz";
+// var idade = 17;
+
+// console.log(`Oi, me chamo ${nwme} e tenho ${idade} anos.`);
+
+/////////////////////////////////////////////////////
+
+
+//var n1 = prompt("Digite um numero: ");
+//var n2 = prompt("Digite outro numero: ");
+
+//var number = parseFloat(n1);
+//var number1 = parseFloat(n2);
+
+//var soma = number + number1;
+//var subtração = number - number1;
+//var multiplicação = number * number1;
+//var divisão = number / number1;
+
+//console.log(`Soma, Subitração, Multiplicação, Divisão do primeiro numero com o segundo resultam em:                                                                                 
+//Soma: ${soma}.                                                                                                                                 
+//Subtração: ${subtração}.                                                                                                               
+//Multiplicação: ${multiplicação}.
+//Divisão: ${divisão}.
+//`)                  
+
+///////////////////////////////////////////////////////
+
+// var idade = prompt("Digite sua idade");
+// var number = parseFloat(idade);
+
+// if(number >= 18){
+//     alert("Seja bem-vindo");
+// }else if(number < 18){
+//     alert("CAI FORA DAQUI!!!");
+// }
+
+// console.log(number / 2);
+
+////////////////////////////////////////////////////////
+
+// for( i = 0; i <= 1000000000000000000000000000000; i ++){
+//     console.log(i)
+// }
